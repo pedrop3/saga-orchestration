5325e55f1ac602b21209edf2a7fcefaae67f5bd4
Pedro Santos <pedro_henriquems321@hotmail.com>
2026-03-12 01:13:22 +0000

    init commit
    Signed-off-by: Pedro Santos <pedro_henriquems321@hotmail.com>

---
+++ ai-saga-agent/src/main/java/com/learn/aisagaagent/AiSagaAgentApplication.java
@@ -0,0 +1,13 @@
+package com.learn.aisagaagent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiSagaAgentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiSagaAgentApplication.class, args);
+	}
+
+}
---
+++ ai-saga-agent/src/test/java/com/learn/aisagaagent/AiSagaAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.aisagaagent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiSagaAgentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
