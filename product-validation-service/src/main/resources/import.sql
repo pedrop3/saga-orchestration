ab82fc372da18cc6b206afbb6c1b15380ca97139
Pedro Santos <pedro_henriquems321@hotmail.com>
2026-03-17 01:32:16 +0000

    review script sql
    Signed-off-by: Pedro Santos <pedro_henriquems321@hotmail.com>

--- product-validation-service/src/main/resources/import.sql
+++ product-validation-service/src/main/resources/import.sql
@@ -1,11 +1,11 @@
 INSERT INTO public.product (id, created_at, updated_at, code)
-VALUES (1, NOW(), NOW(), 'COMIC_BOOKS');
+VALUES (1, NOW(), NOW(), 'COMIC_BOOKS') ON CONFLICT (id) DO NOTHING;
 
 INSERT INTO public.product (id, created_at, updated_at, code)
-VALUES (2, NOW(), NOW(), 'BOOKS');
+VALUES (2, NOW(), NOW(), 'BOOKS') ON CONFLICT (id) DO NOTHING;
 
 INSERT INTO public.product (id, created_at, updated_at, code)
-VALUES (3, NOW(), NOW(), 'MOVIES');
+VALUES (3, NOW(), NOW(), 'MOVIES') ON CONFLICT (id) DO NOTHING;
 
 INSERT INTO public.product (id, created_at, updated_at, code)
-VALUES (4, NOW(), NOW(), 'MUSIC');
+VALUES (4, NOW(), NOW(), 'MUSIC') ON CONFLICT (id) DO NOTHING;
